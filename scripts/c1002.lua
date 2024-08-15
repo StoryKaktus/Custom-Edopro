6b8faa42312467a773a6d584b44130fbb35b461b
StoryKaktus <108303723+StoryKaktus@users.noreply.github.com>
2024-08-16 02:27:32 +0300

    Update c1002.lua

--- scripts/c1002.lua
+++ scripts/c1002.lua
@@ -16,16 +16,6 @@ function s.initial_effect(c)
 	e2:SetCountLimit(1,id)
 	e2:SetOperation(s.fgop)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCountLimit(1,{id,1})
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-	c:RegisterEffect(e3)
 end
 s.listed_names={id}
 function s.fgop(e,tp,eg,ep,ev,re,r,rp)
@@ -40,24 +30,4 @@ function s.fgop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
 	c:RegisterEffect(e2)
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET,0,1)
-end
-function s.decktargets(c,tp)
-	return (c:IsAbleToDeck() or c:IsAbleToExtra()) and c:IsType(TYPE_TUNER) and c:IsFaceup()
-		and Duel.GetMZoneCount(tp,c)>0
-end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc,tp) end
-	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil,tp)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil,tp)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
-end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) or Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==0
-		or not tc:IsLocation(LOCATION_EXTRA+LOCATION_DECK) then return end
-	local c=e:GetHandler()
 end
