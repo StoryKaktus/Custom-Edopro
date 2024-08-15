d8d287b7bd43f3a819fbfbbd563d92307de58bd7
StoryKaktus <108303723+StoryKaktus@users.noreply.github.com>
2024-08-16 02:33:50 +0300

    Update c1002.lua

--- scripts/c1002.lua
+++ scripts/c1002.lua
@@ -1,14 +1,12 @@
 --Faye of the White Forest
 
-local s, id = GetID()
-
+local s,id=GetID()
 function s.initial_effect(c)
-    --Set itself into S/T zones as a spell
+	--Set itself into S/T zones as a spell
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_MONSTER_SSET)
 	e1:SetValue(TYPE_SPELL)
 	c:RegisterEffect(e1)
-	--set open from GY
 end
 s.listed_names={id}
